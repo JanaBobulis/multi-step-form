3b02d897c76364a5f4afe68812443c2e532fe799
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2023-06-25 23:02:26 +0100

    attaching next and prev buttons to steps

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
-
 $( document ).ready(function() {
-    console.log( "ready!" );
+    let stepIndex = $('.sidebar .step-active').index(); 
+    let pageIndex = $('.pages-inner .page-active').index(); 
+    let length = $('.pages-inner .page').length;
+
+    $(".next-step").on("click", function(){
+        $('.sidebar').find('.step-active').removeClass('step-active');
+        stepIndex++;
+         $('.sidebar .step').eq(stepIndex).addClass("step-active");
+
+         $('.pages-inner').find('.page-active').removeClass('page-active');
+         pageIndex++;
+         $('.pages-inner .page').eq(pageIndex).addClass("page-active");
+
+        if(pageIndex >= 1) {
+            $('.previous-step').addClass('previous-step-active')
+        } 
+        if (pageIndex === length) {
+            $('.next-step').addClass('next-step-inactive')
+        }
+    });
+
+    $(".previous-step").on("click", function(){
+        $('.sidebar').find('.step-active').removeClass('step-active');
+        stepIndex--;
+         $('.sidebar .step').eq(stepIndex).addClass("step-active");
+
+         $('.pages-inner').find('.page-active').removeClass('page-active');
+         pageIndex--;
+         $('.pages-inner .page').eq(pageIndex).addClass("page-active");
+
+         console.log(length, pageIndex)
+
+        if(pageIndex < 1) {
+            $('.previous-step').removeClass('previous-step-active')
+        } 
+
+        if (pageIndex < length) {
+            $('.next-step').removeClass('next-step-inactive')
+        }
+
+    });
 });
